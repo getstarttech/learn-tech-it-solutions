239e9bd76f95d557c61b7ecfcadaa375b18705af
learntechitsolution <learntechitsolution@gmail.com>
2025-03-08 23:40:24 +0530

    last commit

--- learn-tech-fe/postbuild.js
+++ learn-tech-fe/postbuild.js
@@ -6,7 +6,7 @@ const errorPagePath = path.join(__dirname, "build", "404.html");
 
 if (fs.existsSync(indexPath)) {
   fs.copyFileSync(indexPath, errorPagePath);
-  console.log("404.html copied successfully");
+  console.log("html copied successfully");
 } else {
   console.error(" Error: index.html not found. Make sure to run 'npm run build' first.");
 }
